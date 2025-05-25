3740bedef9fa202e3b6894dff3dbd5518f34f5c4
foxstudent106431 <t.ulukbek01@gmail.com>
2025-05-25 23:01:49 +0300

    init cobra app structure

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+LoadHound — Relentless SQL load testing tool.
+Copyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com
+
+Licensed under the MIT License.
+*/
+
+package main
+
+import "github.com/Ulukbek-Toichuev/loadhound/cmd"
+
+func main() {
+	cmd.Execute()
+}
