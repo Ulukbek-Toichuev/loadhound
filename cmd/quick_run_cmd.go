17ef09a7d3675f524cd5c0dcd1d1805ae31e8c5b
foxstudent106431 <t.ulukbek01@gmail.com>
2025-06-03 23:33:01 +0300

    add default values as a const

--- cmd/quick_run_cmd.go
+++ cmd/quick_run_cmd.go
@@ -1,8 +1,25 @@
+/*
+LoadHound — Relentless SQL load testing tool.
+Copyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com
+
+Licensed under the MIT License.
+*/
+
 package cmd
 
 import (
+	"time"
+
 	"github.com/Ulukbek-Toichuev/loadhound/internal"
 	"github.com/spf13/cobra"
+)
+
+const (
+	default_workers     int           = 1
+	default_iteration   int           = 1
+	default_duration    time.Duration = 1
+	default_pacing      time.Duration = 1
+	default_output_file string        = "result.json"
 )
 
 func GetQuickRunCmd() *cobra.Command {
@@ -18,11 +35,11 @@ func GetQuickRunCmd() *cobra.Command {
 
 	cmd.Flags().StringVar(&qr.Dsn, "dsn", "", "PostgreSQL DSN connection string (required)")
 	cmd.Flags().StringVar(&qr.Query, "query", "", "SQL query to run (required)")
-	cmd.Flags().IntVar(&qr.Workers, "workers", 1, "Number of concurrent workers")
-	cmd.Flags().IntVar(&qr.Iterations, "iter", 0, "Number of iterations (mutually exclusive with --duration)")
-	cmd.Flags().DurationVar(&qr.Duration, "duration", 0, "Duration of the test (e.g., 10s, 1m)")
-	cmd.Flags().DurationVar(&qr.Pacing, "pacing", 0, "Time between iterations per worker")
-	cmd.Flags().StringVar(&qr.OutputFile, "output-file", "results.json", "File to write test results to")
+	cmd.Flags().IntVar(&qr.Workers, "workers", default_workers, "Number of concurrent workers")
+	cmd.Flags().IntVar(&qr.Iterations, "iter", default_iteration, "Number of iterations (mutually exclusive with --duration)")
+	cmd.Flags().DurationVar(&qr.Duration, "duration", default_duration, "Duration of the test (e.g., 10s, 1m)")
+	cmd.Flags().DurationVar(&qr.Pacing, "pacing", default_pacing, "Time between iterations per worker")
+	cmd.Flags().StringVar(&qr.OutputFile, "output-file", default_output_file, "File to write test results to")
 
 	return cmd
 }
