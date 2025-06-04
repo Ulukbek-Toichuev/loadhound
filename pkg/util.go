b304035e2a6461dd566b41369b3fb55336441b24
foxstudent106431 <t.ulukbek01@gmail.com>
2025-06-04 23:56:11 +0300

    add print ascii art logo

--- pkg/util.go
+++ pkg/util.go
@@ -1,9 +1,18 @@
+/*
+LoadHound — Relentless SQL load testing tool.
+Copyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com
+
+Licensed under the MIT License.
+*/
+
 package pkg
 
 import (
 	"fmt"
 	"math/rand/v2"
 	"os"
+
+	"github.com/common-nighthawk/go-figure"
 )
 
 func randRange(min, max int) int {
@@ -13,4 +22,11 @@ func randRange(min, max int) int {
 func PrintFatal(msg string, err error) {
 	fmt.Printf("%s: %v", msg, err)
 	os.Exit(1)
+}
+
+func PrintAsciiArtLogo() {
+	myFigure := figure.NewColorFigure("LoadHound", "", "red", true)
+	myFigure.Print()
+
+	fmt.Printf("\nLoadHound — Relentless SQL load testing tool.\nCopyright © 2025 Toichuev Ulukbek t.ulukbek01@gmail.com\n\n")
 }
