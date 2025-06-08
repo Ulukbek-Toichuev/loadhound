987fc025c9f866427491f4d3a5df921772dbfbb6
foxstudent106431 <t.ulukbek01@gmail.com>
2025-06-08 16:15:35 +0300

    add feature saving summary stats in file (need improving later)

--- internal/stat/stat_counter.go
+++ internal/stat/stat_counter.go
@@ -8,16 +8,31 @@ Licensed under the MIT License.
 package stat
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 )
-
-// func PrintCurrStat(workerId, iterNum int, queryStat *QueryStat) {
-// 	fmt.Printf("[Worker %d] Iter: %d | Latency: %dms | Rows: %d\n", workerId+1, iterNum, queryStat.Latency.Milliseconds(), queryStat.AffectedRows)
-// }
 
 func PrintSummary(st *SummaryStat) {
 	fmt.Printf("\nLoad Test Summary:\n")
 	fmt.Printf("------------------------\n")
 	fmt.Printf("test start time: %s\ntest end time: %s\nmax response time: %d ms\nmin response time: %d ms\nsuccess queries count: %d\nfailed queries count: %d\ntotal queries count: %d\nworkers count: %d\niteration count: %d\n",
 		st.TestStart.String(), st.TestEnd.String(), st.TotalStat.Max_latency.Milliseconds(), st.TotalStat.Min_latency.Milliseconds(), st.TotalStat.Success, st.TotalStat.Failed, st.TotalStat.Total, st.WorkersCount, st.Iterations)
+}
+
+func SaveInFile(st *SummaryStat, file string) {
+	b, err := json.MarshalIndent(st, "", "  ") // <-- вот тут отличие
+	if err != nil {
+		panic(err)
+	}
+
+	if !strings.HasSuffix(file, ".json") {
+		file = fmt.Sprintf("%s.json", file)
+	}
+
+	err = os.WriteFile(file, b, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
