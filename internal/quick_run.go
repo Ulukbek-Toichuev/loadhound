7fa9d884c83b8836f0043ff51e3e010f8dbfd25d
foxstudent106431 <t.ulukbek01@gmail.com>
2025-06-08 16:16:24 +0300

    bring back field validator feature

--- internal/quick_run.go
+++ internal/quick_run.go
@@ -49,21 +49,65 @@ func NewQuickRun(dsn, query string, workers, iterations int, duration, pacing ti
 
 func QuickRunHandler(ctx context.Context, qr *QuickRun) error {
 	pkg.LogWrapper("Validating quick test parameters...")
-	err := ValidateQuickRunFields(qr)
+	err := validateQuickRunFields(qr)
 	if err != nil {
 		return NewQuickRunErr(fmt.Sprintf("failed to validate fields: %s", err.Error()), err)
 	}
 
 	pkg.LogWrapper("Parsing query template...")
-	tmpl, err := GetTemplate(qr.Query)
+	tmpl, err := ParseQueryTemplate(qr.Query)
 	if err != nil {
 		return NewQuickRunErr("failed to get template", err)
 	}
 
+	fmt.Println("pacing: ", qr.Pacing)
+
 	startTestTime := time.Now()
 	st := ExecuteQuickTest(ctx, qr, tmpl)
 	if st != nil {
-		stat.PrintSummary(stat.NewSummaryStat(st, startTestTime, time.Now(), qr.Workers, qr.Iterations))
+		summary := stat.NewSummaryStat(st, startTestTime, time.Now(), qr.Workers, qr.Iterations)
+		stat.PrintSummary(summary)
+		if qr.OutputFile != "" {
+			stat.SaveInFile(summary, qr.OutputFile)
+		}
+	}
+	return nil
+}
+
+func validateQuickRunFields(qr *QuickRun) error {
+	if qr.Dsn == "" {
+		return NewParseError("--dsn is required", nil)
+	}
+
+	if qr.Query == "" {
+		return NewParseError("--query is required", nil)
+	}
+
+	if qr.Workers < 0 {
+		return NewParseError("--workers must be >= 0", nil)
+	}
+
+	iterations := qr.Iterations
+	duration := qr.Duration
+
+	if iterations < 0 {
+		return NewParseError("--iterations must be >= 0", nil)
+	}
+
+	if duration < 0 {
+		return NewParseError("--duration must be >= 0", nil)
+	}
+
+	if iterations == 0 && duration == 0 {
+		return NewParseError("either --iter or --duration must be set", nil)
+	}
+
+	if iterations > 0 && duration > 0 {
+		return NewParseError("--iter and --duration are mutually exclusive", nil)
+	}
+
+	if qr.Pacing < 0 {
+		return NewParseError("--pacing must be > 0", nil)
 	}
 	return nil
 }
