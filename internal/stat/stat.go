ac73049d6e292e89fa54c52186d9f17de142b96e
foxstudent106431 <t.ulukbek01@gmail.com>
2025-06-09 15:13:42 +0300

    improve stats implementation

--- internal/stat/stat.go
+++ internal/stat/stat.go
@@ -8,6 +8,8 @@ Licensed under the MIT License.
 package stat
 
 import (
+	"math"
+	"sort"
 	"sync"
 	"time"
 )
@@ -23,16 +25,16 @@ func NewQueryStat(latency time.Duration, err error, affectedRows int64) *QuerySt
 }
 
 type Stat struct {
-	mu          sync.Mutex
-	Success     int
-	Failed      int
-	Total       int
-	Min_latency time.Duration
-	Max_latency time.Duration
+	mu              sync.Mutex
+	Success         int
+	Failed          int
+	Total           int
+	QueriesRespList []int64
+	ErrMap          map[string]int
 }
 
 func NewStat() *Stat {
-	return &Stat{mu: sync.Mutex{}}
+	return &Stat{mu: sync.Mutex{}, QueriesRespList: make([]int64, 0), ErrMap: make(map[string]int)}
 }
 
 func (s *Stat) SubmitStat(qr *QueryStat) {
@@ -41,31 +43,109 @@ func (s *Stat) SubmitStat(qr *QueryStat) {
 
 	if qr.Err != nil {
 		s.Failed++
+		s.ErrMap[qr.Err.Error()]++
 	} else {
 		s.Success++
-	}
-
-	if qr.Latency < s.Min_latency && s.Min_latency != 0 {
-		s.Min_latency = qr.Latency
-	} else if qr.Latency > s.Max_latency {
-		s.Max_latency = qr.Latency
-	}
-
-	if s.Min_latency == 0 {
-		s.Min_latency = qr.Latency
+		s.QueriesRespList = append(s.QueriesRespList, qr.Latency.Milliseconds())
 	}
 
 	s.Total++
 }
 
-type SummaryStat struct {
-	TestStart    time.Time
-	TestEnd      time.Time
-	TotalStat    *Stat
-	WorkersCount int
-	Iterations   int
+type Result struct {
+	Start          time.Time `json:"start"`
+	End            time.Time `json:"end"`
+	TotalTime      time.Time `json:"total_time"`
+	TotalQueries   int       `json:"total_queries"`
+	SuccessQueries int       `json:"successfull_queries"`
+	FailedQueries  int       `json:"failed_queries"`
+	Throughput     float64   `json:"throuphput"`
+	Latency        *Latency  `json:"latency"`
+	TopErrors      []string  `json:"top_errors"`
 }
 
-func NewSummaryStat(totalStat *Stat, testStart, testEnd time.Time, workersCount, iterations int) *SummaryStat {
-	return &SummaryStat{testStart, testEnd, totalStat, workersCount, iterations}
+type Latency struct {
+	Min    int64   `json:"min"`
+	Max    int64   `json:"max"`
+	Avg    float64 `json:"avg"`
+	Median int64   `json:"median"`
+	P90    int64   `json:"p90"`
+	P95    int64   `json:"p95"`
+	P99    int64   `json:"p99"`
+}
+
+func NewLatency(min, max, median, p90, p95, p99 int64, avg float64) *Latency {
+	return &Latency{min, max, avg, median, p90, p95, p99}
+}
+
+func GetResult(start, end time.Time, total time.Duration, stat *Stat) *Result {
+	var result Result
+	result.Start = start
+	result.End = end
+	result.TotalQueries = stat.Total
+	result.SuccessQueries = stat.Success
+	result.FailedQueries = stat.Failed
+	result.Latency = GetLatency(stat.QueriesRespList)
+	result.Throughput = throughput(len(stat.QueriesRespList), total)
+	return &result
+}
+
+func GetLatency(data []int64) *Latency {
+	var latency Latency
+	if len(data) == 0 {
+		return &latency
+	}
+
+	sorted := make([]int64, len(data))
+	copy(sorted, data)
+	sort.Slice(sorted, func(i, j int) bool { return sorted[i] < sorted[j] })
+
+	min, max := minMax(sorted)
+	avg := average(sorted)
+	md := median(sorted)
+	p90 := percentile(sorted, 90)
+	p95 := percentile(sorted, 95)
+	p99 := percentile(sorted, 99)
+	return NewLatency(min, max, md, p90, p95, p99, avg)
+}
+
+func minMax(data []int64) (int64, int64) {
+	return data[0], data[len(data)-1]
+}
+
+func average(data []int64) float64 {
+	var sum int64
+	for _, d := range data {
+		sum += d
+	}
+	return float64(sum / int64(len(data)))
+}
+
+func median(data []int64) int64 {
+	n := len(data)
+	if n%2 == 1 {
+		return data[n/2]
+	} else {
+		md1 := data[n/2-1]
+		md2 := data[n/2]
+		md := (md1 + md2) / 2.0
+		return md
+	}
+}
+
+func percentile(data []int64, p float64) int64 {
+	n := len(data)
+	index := int(math.Ceil(p/100*float64(n))) - 1
+	if index < 0 {
+		index = 0
+	}
+	if index >= n {
+		index = n - 1
+	}
+	return data[index]
+}
+
+func throughput(numRequests int, totalTime time.Duration) float64 {
+	seconds := totalTime.Seconds()
+	return float64(numRequests) / seconds
 }
